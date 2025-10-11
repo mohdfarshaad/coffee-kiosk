f8a0f6b9fef9eba15374ddb26f9eb186169268bc
Mohammed Farshad <imohammedfarshad@gmail.com>
2025-10-11 11:10:07 +0100

    create simple jwt token Auth
    Signed-off-by: Mohammed Farshad <imohammedfarshad@gmail.com>

---
+++ kiosk-backend/src/auth.py
@@ -0,0 +1,17 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+import jwt
+
+SECRET_KEY = "SECRET123"
+ALGORITHM = "HS256"
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+def verify_token(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except jwt.PyJWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or expired token",
+        )
