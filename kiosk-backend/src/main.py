9d68e48d8a33a7f8d33a960986a4dc2b1c23fea3
Mohammed Farshad <imohammedfarshad@gmail.com>
2025-10-11 16:42:37 +0530

    add cors

--- kiosk-backend/src/main.py
+++ kiosk-backend/src/main.py
@@ -1,14 +1,25 @@
 from fastapi import FastAPI
 from . import models, database
 from .routers import drinks, orders
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI(title="Kiosk Raspberry Pi GPIO Service",version="0.1")
+app = FastAPI(title="Kiosk Raspberry Pi GPIO Service", version="0.1")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or restrict to your frontend's origin
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 models.Base.metadata.create_all(bind=database.engine)
 
 app.include_router(drinks.router)
 app.include_router(orders.router)
 
+
 @app.get("/")
 def read_root():
-    return {"message":"Server is Running "}
+    return {"message": "Server is Running "}
