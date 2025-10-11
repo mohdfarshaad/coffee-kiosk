a425074175b1d1900d06934692d7b9d9f7aa6ea1
Mohammed Farshad <imohammedfarshad@gmail.com>
2025-10-11 11:11:20 +0100

    integrate DB Engine and routers
    Signed-off-by: Mohammed Farshad <imohammedfarshad@gmail.com>

--- kiosk-backend/src/main.py
+++ kiosk-backend/src/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from . import models, database
+from .routers import drinks, orders
 
 app = FastAPI(title="Kiosk Raspberry Pi GPIO Service",version="0.1")
+
+models.Base.metadata.create_all(bind=database.engine)
+
+app.include_router(drinks.router)
+app.include_router(orders.router)
 
 @app.get("/")
 def read_root():
